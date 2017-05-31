41ceeaa098238a8557ae572d32c40632c3934e2e
Zach Collins <recursive.cookie.jar@gmail.com>
2017-06-01 08:56:27 +0900

    uploader finished

--- packages/dropbox_uploader.nix
+++ packages/dropbox_uploader.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/bin
     chmod +x dropbox_uploader.sh
-    cp dropbox_uploader.sh $out/bin/
+    cp dropbox_uploader.sh $out/bin/dropbox_uploader
     for prg in $out/bin"/"*;do
       wrapProgram $prg --set PATH "${bash}/bin:${curl}/bin:${coreutils}/bin:${gnused}/bin:${which}/bin:${gnugrep}/bin"
     done
