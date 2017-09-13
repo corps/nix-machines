ca3f9de0aab4349845b5af1f0699d10bffb0cfe4
Zach Collins <recursive.cookie.jar@gmail.com>
2017-09-13 17:33:11 +0900

    Remove vim scripts, add fetch-from-github

--- config.nix
+++ config.nix
@@ -4,12 +4,13 @@
         pkg = pkgs.callPackage;
     in 
   with self; rec {
-    dropbox_uploader = pkg packages/dropbox_uploader.nix {};
-    ngrok = pkg packages/ngrok.nix {};
-    xhelpers = pkg packages/xhelpers.nix {};
-    corpscripts = pkg packages/scripts.nix {};
-    freeciv = pkg packages/freeciv.nix {};
-    my_neovim = pkg packages/vim {};
+    dropbox_uploader = pkg ./packages/dropbox_uploader.nix {};
+    ngrok = pkg ./packages/ngrok.nix {};
+    xhelpers = pkg ./packages/xhelpers.nix {};
+    corpscripts = pkg ./packages/scripts.nix {};
+    freeciv = pkg ./packages/freeciv.nix {};
+    my_neovim = pkg ./packages/vim {};
+    fetch_from_github = pkg ./packages/fetch-from-github.nix {};
   };
 
   allowUnfree = true;
