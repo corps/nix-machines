dda559cd0c4a295cd82199df0f48b75655f946a9
Zach Collins <recursive.cookie.jar@gmail.com>
2017-09-14 13:43:19 +0900

    Adding uglifyjs

--- config.nix
+++ config.nix
@@ -1,8 +1,8 @@
-{ pkgs }: { 
-  packageOverrides = super: 
-    let self = super.pkgs; 
+{ pkgs }: {
+  packageOverrides = super:
+    let self = super.pkgs;
         pkg = pkgs.callPackage;
-    in 
+    in
   with self; rec {
     dropbox_uploader = pkg ./packages/dropbox_uploader.nix {};
     ngrok = pkg ./packages/ngrok.nix {};
@@ -11,6 +11,7 @@
     freeciv = pkg ./packages/freeciv.nix {};
     my_neovim = pkg ./packages/vim {};
     fetch_from_github = pkg ./packages/fetch-from-github.nix {};
+    uglifyjs = (pkg ./packages/uglifyjs {})."uglify-js-3.1.0";
   };
 
   allowUnfree = true;
