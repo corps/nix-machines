ee12c39886f7aa949ac224cd8726c174fae516e6
Zach Collins <recursive.cookie.jar@gmail.com>
2017-11-11 14:19:54 -0800

    More defaults.

--- modules/darwin/common.nix
+++ modules/darwin/common.nix
@@ -1,4 +1,11 @@
 {
   environment.variables.EDITOR = "vim";
   environment.variables.LANG = "en_US.UTF-8";
+
+  system.defaults.NSGlobalDomain."com.apple.trackpad.trackpadCornerClickBehavior" = 1;
+  system.defaults.NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.orientation = "left";
+  system.defaults.finder.AppleShowAllExtensions = true;
+  system.defaults.finder._FXShowPosixPathInTitle = true;
 }
