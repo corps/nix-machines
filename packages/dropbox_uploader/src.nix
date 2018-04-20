4d84c748a895c16a0de1a60dc173e492a4af2297
Zach Collins <recursive.cookie.jar@gmail.com>
2018-04-20 09:21:01 -0700

    Wip

---
+++ packages/dropbox_uploader/src.nix
@@ -0,0 +1,6 @@
+{ fetchFromGitHub }: fetchFromGitHub {
+  owner = "andreafabrizi";
+  repo = "Dropbox-Uploader";
+  rev = "33e87ee15ac6b92f9533a8b028c5a3416328088c";
+  sha256 = "11b2hz3vliaagc052rfdyr2xsmhmyzi2vmydrz4jh9wsrrva62qy";
+}
