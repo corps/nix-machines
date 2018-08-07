44fcda23f88968f15061518bc28ccd31d8b43932
Zachary Collins <recursive.cookie.jar@gmail.com>
2018-08-06 22:14:11 -0700

    Trying out space emacs

--- surface-book.nix
+++ surface-book.nix
@@ -10,6 +10,7 @@
     universal-ctags ag pkgs.nodePackages.node2nix
     gnupg qrcode-svg fetch_from_pypi git-dropbox wintmp
     terminator font-manager xorg.libXrender jre8
+    emacs
   ];
 
   programs.autohotkey.scripts = [
@@ -17,6 +18,12 @@
   ];
 
   system.symlinks."$HOME/.dir_colors" = toString ./dotfiles/dircolors.256dark;
+
+  system.activationScripts.extraUserActivation.text = ''
+    set -x
+    [[ ! -e ~/.emacs.d ]] && git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+    set +x
+  '';
 
   # system.activationScripts.extraActivation.text = ''
   # sed -i 's$<listen>.*</listen>$<listen>tcp:host=localhost,port=0</listen>$' /etc/dbus-1/session.conf
