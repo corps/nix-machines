5ee10bf99d5e8b864bff4206497ec2ed61912583
Zach Collins <recursive.cookie.jar@gmail.com>
2018-08-21 11:58:23 -0700

    Indirection to fix nix-env -qa

--- packages/docstore/default.nix
+++ packages/docstore/default.nix
@@ -1,2 +1,18 @@
-{ callPackage }:
-import (callPackage ./package.nix {}) {}
+{ callPackage, stdenv, nix }:
+
+let
+  inner = import (callPackage ./package.nix {}) {};
+in
+
+stdenv.mkDerivation {
+  name = "docstore";
+
+  phases = [ "installPhase" ];
+
+  inherit inner;
+
+  installPhase = ''
+    mkdir $out
+    cp --preserve=links -r $inner/* $out/
+  '';
+}
