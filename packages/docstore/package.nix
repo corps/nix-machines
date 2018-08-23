bdeddc5f9b1be0833a6577101d213a6124436422
Zachary Collins <recursive.cookie.jar@gmail.com>
2018-08-23 03:04:20 -0700

    Upgrading to fixed docstore

--- packages/docstore/package.nix
+++ packages/docstore/package.nix
@@ -2,6 +2,6 @@
 fetchFromGitHub {
   owner = "corps";
   repo = "docstore";
-  rev = "db8916ad8447d2286ee3404f122c9bbcc6606baa";
-  sha256 = "1am4cd02dnybm0ndx296k6nfzzfjjaacl5nv6h2ypqdk1fwnc8c0";
+  rev = "6c75841ee4b95e3830c5c9eb6bdf987c9d647ef5";
+  sha256 = "1km1iqxnqra3my5m3nfnzcp556ndph85gic1n6n9zfmlbmgqpkrq";
 }
