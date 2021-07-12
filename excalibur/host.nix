325beb793a0915cc29ca01bf481aeed67f4cba85
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-07-11 22:06:30 -0700

    Firewall adjustment

--- excalibur/host.nix
+++ excalibur/host.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
   imports = [ ../modules/nixos/workstation.nix ];
-  networking.firewall.allowedTCPPorts = [ 7100 9323 9100 ];
+  networking.firewall.allowedTCPPorts = [ 7100 9323 9100 9700 ];
   programs.steam.enable = true;
 }
