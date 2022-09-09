7d7a5b923e7b8673d7f6903a5b1c60d7e7717079
Zachary Collins <recursive.cookie.jar@gmail.com>
2022-09-09 00:13:55 -0700

    Adding trusted interfaces

--- excalibur/host.nix
+++ excalibur/host.nix
@@ -1,4 +1,12 @@
 { config, pkgs, ... }:
 {
   imports = [ ../modules/nixos/server.nix ../modules/nixos/swarm-host.nix ];
+  services.nfs.server.enable = true;
+  services.nfs.server.exports = ''
+    /export         10.0.0.14(rw,fsid=0,no_subtree_check) 10.0.0.115(rw,fsid=0,no_subtree_check) 10.0.0.45(rw,fsid=0,no_subtree_check)
+    /export/data  10.0.0.14(rw,nohide,insecure,no_subtree_check) 10.0.0.115(rw,nohide,insecure,no_subtree_check) 10.0.0.45(rw,nohide,insecure,no_subtree_check)
+  '';
+
+  networking.firewall.allowedTCPPorts = [ 111 2049 ];
+  networking.firewall.allowedUDPPorts = [ 111 2049 ];
 }
--- modules/nixos/swarm-host.nix
+++ modules/nixos/swarm-host.nix
@@ -2,6 +2,7 @@
 {
   networking.firewall.allowedTCPPorts = [ 2377 7946 ];
   networking.firewall.allowedUDPPorts = [ 4789 7946 ];
+  networking.firewall.trustedInterfaces = [ "docker0" "docker_gwbridge" ];
 
   fileSystems."/mnt/data" = {
     device = "10.0.0.115:/data";
