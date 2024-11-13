a242662f6da8112d2b25dcea18a35daff01a4e94
Zachary Collins <recursive.cookie.jar@gmail.com>
2024-11-13 00:18:16 -0800

    More fixes

--- excalibur/home.nix
+++ excalibur/home.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
+
+let unstable = import <unstable> {}; in
+
 {
   imports = [ ../modules/home/workstation.nix ];
   
+  home.packages = with unstable; [
+    (jetbrains.plugins.addPlugins jetbrains.pycharm-professional [ "nixidea" "ideavim" ])
+  ];
   home.stateVersion = "20.09";
 }
