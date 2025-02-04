5ff83f0d658f95cbd4db164863fcc7f155b4a4d0
Zachary <recursive.cookie.jar@gmail.com>
2025-02-03 17:12:39 -0800

    Adding lean completions, some wip

--- modules/lean.nix
+++ modules/lean.nix
@@ -10,5 +10,9 @@
 
   config = lib.mkIf config.environment.development.enable {
     environment.systemPackages = with pkgs; [ elan ];
+
+    programs.bash.interactiveShellInit = ''
+      source <(elan completions bash)
+    '';
   };
 }
