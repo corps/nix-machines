68294d359d9b0f435a8ddc56255f91311d8d3d02
Zachary <recursive.cookie.jar@gmail.com>
2025-02-10 01:09:34 -0800

    Return to using remote vim

--- bin/mistral.sh
+++ bin/mistral.sh
@@ -1,4 +1,44 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i bash -p jq
 
-echo $(which jq)
+API_URL="https://api.mistral.ai/v1/completions"
+API_KEY="YOUR_API_KEY"
+MODEL="open-mistral-nemo-2407"
+MAX_TOKENS="1024"
+CHAT_LOG=/tmp/mistral.chat.log
+touch $CHAT_LOG
+CHAT_HISTORY="$(cat $CHAT_LOG)"
+
+while getopts ":v" opt; do
+  case $opt in
+  c)
+    echo "" >$CHAT_LOG
+    CHAT_HISTORY='[{"role": "system", "content": "You are a professional software engineer."}]'
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+USER_MESSAGE="$(jq -n --arg message "$1" '[{"role": "user", "content": $message}]')"
+
+CHAT_HISTORY="$(jq -s '[.]]' <(echo "$CHAT_HISTORY
+$USER_MESSAGE"))"
+
+# Use jq to format the JSON data
+# JSON_DATA="$(jq -n \
+#   --argjson max_tokens "$MAX_TOKENS" \
+#   --argjson messages "$CHAT_HISTORY" \
+#   --arg model "$MODEL" \
+#   '{
+#     "model": $model,
+#     "messages": $messages,
+#     "max_tokens": $max_tokens
+#   }')"
+
+echo $CHAT_HISTORY
+
+# curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $MISTRAL_API_KEY" -d "$JSON_DATA" "$API_URL"
--- excalibur/home.nix
+++ excalibur/home.nix
@@ -5,4 +5,19 @@
   environment.development.enable = true;
 
   imports = [ ../modules/home.nix ];
+
+  systemd.user.services.nvim-server = {
+    enable = true;
+    unitConfig = {
+      type = "Simple";
+    };
+
+    serviceConfig = {
+      Restart = "always";
+      ExecStart = "/usr/bin/env bash -l -c nvim --listen 0.0.0.0:8991 --headless";
+    };
+
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+  };
 }
