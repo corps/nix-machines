f5c1ebb48016bd2506341ffe96c3e9051d9c94a2
Zachary Collins <recursive.cookie.jar@gmail.com>
2026-05-17 03:07:34 -0700

    Updated keybindings

--- imac/default.nix
+++ imac/default.nix
@@ -13,7 +13,7 @@
   services.skhd.skhdConfig = ''
     shift + cmd - d    : open -a "Safari"
     shift + cmd - e    : open -a "Alacritty"
-    shift + cmd - 0x2C : open -a "Visual Studio Code"
+    shift + cmd - 0x2C : open -a "PyCharm"
     shift + cmd - 0x2F : open -a Neovide
   '';
 
