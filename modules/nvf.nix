65a293e02a63009884e88a10eabe48105318fcdc
Zachary <recursive.cookie.jar@gmail.com>
2026-08-02 16:49:36 -0700

    nvf is pretty coool

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -1,16 +1,78 @@
-{ pkgs, inputs }:
-
 {
+  pkgs,
+  inputs,
+  ...
+}: {
   environment.systemPackages = [
     (inputs.nvf.lib.neovimConfiguration {
       inherit pkgs;
       modules = [
         {
           config.vim = {
-            # Enable custom theming options
+            opts.expandtab = true;
+            opts.tabstop = 2;
+            opts.shiftwidth = 2;
+            opts.softtabstop = 2;
+
+            lsp = {
+              enable = true;
+              formatOnSave = true;
+
+              # How to codeactions?
+              lightbulb.enable = true;
+            };
+
             theme.enable = true;
-            # Enable Treesitter
             treesitter.enable = true;
+            treesitter.context.enable = true;
+
+            tabline = {
+              nvimBufferline.enable = true;
+            };
+
+            filetree = {
+              neo-tree = {
+                enable = true;
+              };
+            };
+
+            binds = {
+              whichKey.enable = true;
+              whichKey.register = {
+                "<leader>l" = "Lsp Actions";
+              };
+              cheatsheet.enable = true;
+            };
+
+            languages = {
+              enableFormat = true;
+              enableTreesitter = true;
+              enableExtraDiagnostics = true;
+
+              python = {
+                enable = true;
+              };
+
+              nix = {
+                enable = true;
+              };
+
+              env = {
+                enable = true;
+              };
+
+              docker = {
+                enable = true;
+              };
+
+              typescript = {
+                enable = true;
+              };
+
+              cmake = {
+                enable = true;
+              };
+            };
           };
         }
       ];
