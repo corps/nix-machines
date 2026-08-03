bcd58bd4665a6c0b2ffad10f2715fbe05a5439c2
Zachary <recursive.cookie.jar@gmail.com>
2026-08-02 22:39:48 -0700

    More adjustments

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -2,17 +2,109 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  keymaps = [
+    {
+      key = "<d-c>";
+      mode = "n";
+      silent = true;
+      action = "\"*y :let @+=@*<CR>";
+    }
+    {
+      key = "<d-v>";
+      mode = "n";
+      silent = true;
+      action = "+p";
+    }
+    {
+      key = "<d-v>";
+      mode = "i";
+      silent = true;
+      action = "<C-R>+";
+    }
+    {
+      key = "<d-{>";
+      mode = "n";
+      silent = true;
+      action = ":BufferLineCyclePrev<CR>";
+    }
+    {
+      key = "<d-}>";
+      mode = "n";
+      silent = true;
+      action = ":BufferLineCycleNext<CR>";
+    }
+    {
+      key = "<d-w>";
+      mode = "n";
+      silent = true;
+      action = ":bd<CR>";
+    }
+
+    {
+      key = "<leader>{";
+      mode = "n";
+      silent = true;
+      action = ":BufferLineCyclePrev<CR>";
+    }
+    {
+      key = "<leader>}";
+      mode = "n";
+      silent = true;
+      action = ":BufferLineCycleNext<CR>";
+    }
+    {
+      key = "<leader>w";
+      mode = "n";
+      silent = true;
+      action = ":bd<CR>";
+    }
+    {
+      key = "<d-/>";
+      mode = "v";
+      silent = true;
+      action = "gc";
+    }
+    {
+      key = "<d-/>";
+      mode = "n";
+      silent = true;
+      action = "gcc";
+    }
+    {
+      key = "<d-[>";
+      mode = "n";
+      silent = true;
+      action = "<C-o>";
+    }
+    {
+      key = "<d-]>";
+      mode = "n";
+      silent = true;
+      action = "<C-i>";
+    }
+    {
+      key = "<d-b>";
+      mode = "n";
+      silent = true;
+      action = "<leader>lgd";
+    }
+  ];
+in {
   environment.systemPackages = [
     (inputs.nvf.lib.neovimConfiguration {
       inherit pkgs;
       modules = [
         {
           config.vim = {
-            opts.expandtab = true;
-            opts.tabstop = 2;
-            opts.shiftwidth = 2;
-            opts.softtabstop = 2;
+            inherit keymaps;
+
+            opts = {
+              expandtab = true;
+              tabstop = 2;
+              shiftwidth = 2;
+              softtabstop = 2;
+            };
 
             lsp = {
               enable = true;
@@ -20,6 +112,7 @@
 
               # How to codeactions?
               lightbulb.enable = true;
+              trouble.enable = true;
             };
 
             theme.enable = true;
@@ -34,6 +127,10 @@
               neo-tree = {
                 enable = true;
               };
+            };
+
+            autocomplete.nvim-cmp = {
+              enable = true;
             };
 
             binds = {
